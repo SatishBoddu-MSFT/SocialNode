3d43ac478bf454a8169616ebf59768a1ab4db2e6
SatishBoddu <62923178+SatishBoddu-MSFT@users.noreply.github.com>
2022-03-25 12:10:47 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendTelemetryDotNetFramework
+{
+    class Program
+    {
+        private static DeviceClient s_deviceClient;
+        private readonly static string s_connectionString01 = "HostName=iothub.azure-devices.net;DeviceId=test;SharedAccessKey=82yEPTs=";
+        static void Main(string[] args)
+        {
+            s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString01, TransportType.Mqtt);
+            SendDeviceToCloudMessagesAsync(s_deviceClient);
+
+            var asm = Assembly.GetExecutingAssembly();
+            var b = asm.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(TargetFrameworkAttribute));
+            var strFramework = b.NamedArguments[0].TypedValue.Value;
+
+            Console.WriteLine(strFramework);
+
+          
+            Console.ReadLine();
+
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync(DeviceClient s_deviceClient)
+        {
+            try
+            {
+                double minTemperature = 20;
+                double minHumidity = 60;
+                Random rand = new Random();
+
+                while (true)
+                {
+                    double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                    double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                    // Create JSON message  
+
+                    var telemetryDataPoint = new
+                    {
+
+                        temperature = currentTemperature,
+                        humidity = currentHumidity
+                    };
+
+                    string messageString = "";
+
+
+
+                    messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+
+                    var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                    // Add a custom application property to the message.  
+                    // An IoT hub can filter on these properties without access to the message body.  
+                    //message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");  
+
+                    // Send the telemetry message  
+                    await s_deviceClient.SendEventAsync(message);
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                    await Task.Delay(1000 * 10);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
